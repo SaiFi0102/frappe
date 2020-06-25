f466c78247761034d12f4dd8a0bb069c649f6603
Saif Ur Rehman <saifi0102@gmail.com>
2020-06-25 16:30:34 +0500

    fix: Website / Customer Portal fixes and v12 upgrade

--- frappe/www/list.py
+++ frappe/www/list.py
@@ -18,7 +18,7 @@ def get_context(context, **dict_params):
 	context.meta = frappe.get_meta(doctype)
 	context.update(get_list_context(context, doctype) or {})
 	context.doctype = doctype
-	context.title = context.doctype
+	context.title = context.get('title') or context.doctype
 	context.txt = frappe.local.form_dict.txt
 	context.update(get(**frappe.local.form_dict))
 
