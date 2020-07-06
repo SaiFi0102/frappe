10066fbb7d9811ebd1000d96b05bc961d1f3776e
Saif Ur Rehman <saifi0102@gmail.com>
2020-07-06 17:25:57 +0500

    fix: get_cached_value in print

--- frappe/utils/safe_exec.py
+++ frappe/utils/safe_exec.py
@@ -57,6 +57,7 @@ def get_safe_globals():
 			get_meta = frappe.get_meta,
 			get_doc = frappe.get_doc,
 			get_cached_doc = frappe.get_cached_doc,
+			get_cached_value = frappe.get_cached_value,
 			get_list = frappe.get_list,
 			get_all = frappe.get_all,
 			get_system_settings = frappe.get_system_settings,
